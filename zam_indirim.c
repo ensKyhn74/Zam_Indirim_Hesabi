6773b457559dfbcb0e8a9ec658594678b5ce2a39
ensKyhn74 <117343032+ensKyhn74@users.noreply.github.com>
2022-11-03 23:54:22 +0300

    Add files via upload

---
+++ zam_indirim.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+// klavyeden girilen harfe göre zam veya indirim hesaplayıp ürünün yeni fiyatını hesaplar
+
+int main() 
+
+{
+ 
+ double UrunFiyati;
+ double indirim,zam;
+ char secim; 
+ 
+ printf("urunun fiyatini girin: ");
+ scanf("%lf",&UrunFiyati); 
+ 
+ printf("zam mi yapacaksiniz indirim mi?: ");
+ scanf(" %c",&secim); // bir boşluğa dikkat et! aksi halde output hatalı olur
+ 
+  if(secim == 'i' || secim == 'I')
+  {
+    printf("%% kac indirim yapacaksiniz?: "); // %% bir tane % yazdırılmasını sağlar
+    scanf("%lf",&indirim); 
+    
+    if(indirim > 0 && indirim <= 100)
+    {
+      double yeniFiyat = UrunFiyati - (UrunFiyati * (indirim / 100)); 
+      printf("urunun indirimli fiyati: %.2lf",yeniFiyat); 
+    }
+     
+    else
+    {
+     printf("%% 100'den fazla ve %% 1'den az indirim yapilamaz!");
+    }
+  }
+  
+   if(secim == 'z' || secim == 'Z')
+   {
+     printf("%% kac zam yapacaksiniz?: ");
+     scanf("%lf",&zam); 
+     
+    if(zam > 0)
+    {
+     double YeniFiyat = UrunFiyati + (UrunFiyati * (zam / 100)); 
+     printf("urunun zamli fiyati: %.2lf",YeniFiyat); 
+    }
+     
+    else
+    {
+     printf("%% 1'den az zam yapilamaz!");	
+    }
+   }
+ 
+   return 0;
+
+}
